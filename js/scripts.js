21aa4de24ede27fb619d53e603674d0e078a2704
Eric Endsley <eric.endsley4@gmail.com>
2020-10-21 16:54:56 -0700

    initial commit
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>
    co-authored-by: Randel Moore <randel.c.moore@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log("is this working?");
+  $("form#dog").submit(function(event) {
+    console.log("is this working?");
+    const name = $("input#name").val();
+    const size = $("input#bag-size").val();
+    const protein = $("input:radio[name=protein]: checked").val();
+    const delivery = $("input#delivery").val();
+
+    $(".name").text(name);
+    $(".bag-size").text(size);
+    $(".protein").text(protein);
+    $(".delivery").text(delivery);
+
+    //("#receipt").show();
+
+    event.preventDefault();
+  });
+});
